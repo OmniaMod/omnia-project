85489e9eef9479e0bb7809aabedcfceeff6f7024
OmniaMod <omnia4lyfe@gmail.com>
2024-12-04 16:12:36 -0800

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({"message": "OMNIA is live and pulling insights from the zeitgeist!"})
-
-@app.route('/get-twitter-trends', methods=['GET'])
-def get_twitter_trends():
-    return jsonify({"message": "Twitter trends fetched successfully"})
-
 import os
 import tweepy
 from flask import Flask, jsonify
